80f51086ae5faee5207b0c028e67d68ad5cba8a2
Alejandro Rizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-12-02 08:51:53 -0300

    Create 022_nros_unicos.r

---
+++ 022_nros_unicos.r
@@ -0,0 +1,12 @@
+"Write a R program to find the maximum and the minimum value of a given vector"
+
+# Número límite
+c <- c(1,4,7,2,6,5,1,4,8,9,10,2,3,5,2,1,4)
+x <- unique(c)
+print(c)
+print(x)
+
+"
+ [1]  1  4  7  2  6  5  1  4  8  9 10  2  3  5  2  1  4
+ [1]  1  4  7  2  6  5  8  9 10  3
+"

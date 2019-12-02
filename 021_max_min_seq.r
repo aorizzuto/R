cf98d95ac6a810bc621fff56464ae4723e76520f
Alejandro Rizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-12-02 08:42:39 -0300

    Create 021_max_min_seq.r

---
+++ 021_max_min_seq.r
@@ -0,0 +1,14 @@
+"Write a R program to find the maximum and the minimum value of a given vector"
+
+# Número límite
+c <- sample(-100:100,20, replace=T)
+
+print(c)
+
+print(paste("Min:",min(c), " Max:", max(c)))
+
+"
+ [1]   3  89 -85 -95 -95 -38 -69 -54 -13 -79 -86   3  82   9  76   3 -36  69  58
+[20] -68
+[1] "Min: -95  Max: 89"
+"

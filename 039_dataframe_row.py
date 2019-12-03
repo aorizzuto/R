5b4f31921103d0ccb9885c5090c0c81f613e8e67
Alejandro Rizzuto <45216345+aorizzuto@users.noreply.github.com>
2019-12-03 07:22:06 -0300

    Create 039_dataframe_row.py

---
+++ 039_dataframe_row.py
@@ -0,0 +1,48 @@
+"Write a R program to add a new column in a given data frame"
+
+a <- c(1,1,1,1,1)
+b <- rep(2,5)
+c <- rep(3,5)
+d <- rep(4,5)
+
+e <- rep(5,4)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+df = data.frame(a,b,c,d)
+
+cat("\nData Frame antes de agregar columna:\n")
+print(df)
+
+df["e", ] <- e
+cat("\nData Frame nueva:\n")
+print(df)
+
+"
+[1] 1 1 1 1 1
+[1] 2 2 2 2 2
+[1] 3 3 3 3 3
+[1] 4 4 4 4 4
+[1] 5 5 5 5
+
+Data Frame antes de agregar columna:
+  a b c d
+1 1 2 3 4
+2 1 2 3 4
+3 1 2 3 4
+4 1 2 3 4
+5 1 2 3 4
+
+Data Frame nueva:
+  a b c d
+1 1 2 3 4
+2 1 2 3 4
+3 1 2 3 4
+4 1 2 3 4
+5 1 2 3 4
+e 5 5 5 5
+"
